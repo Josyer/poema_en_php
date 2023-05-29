be581306ee9be1c5949e63dd79f2b289ee30f29d
Josyer <126198276+Josyer@users.noreply.github.com>
2023-05-29 11:02:18 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<pre>
+	<?php
+	echo "Recuerde el alma dormida
+	Avive el seso y despierte
+	Contemplando
+	Cómo se pasa la vida,
+	Cómo se viene la muerte,
+	Tan callando,
+	Cuán presto se va el placer,
+	Cómo, después de acordado
+	Da dolor,
+	Cómo, a nuestro parecer,
+	Cualquier tiempo pasado
+	Fue mejor.";
+	?>
+	</pre>
+</body>
+</html>
